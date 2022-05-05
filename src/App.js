fbb8fb92657ebc2293987f83b94507a76a5d1882
yessin93 <mohamedyessin.bouchaala93@gmail.com>
2022-05-05 18:54:14 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './components/First';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Second from './components/Second';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> my first application </h1>
+      <First/>
+      <Second/>
+    
+    </div>
+  );
+}
+
+export default App;
